0a0c3cb7613e8eb28349bc9930f14c112efb56b3
Azamat-JS <azamatielts987@gmail.com>
2025-12-22 22:52:46 +0500

    created back button in settings page

--- flutter_app/lib/views/pages/settings_page.dart
+++ flutter_app/lib/views/pages/settings_page.dart
@@ -16,7 +16,15 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
+      appBar: AppBar(
+        title: Text('Settings'),
+        leading: BackButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        automaticallyImplyLeading: false,
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
