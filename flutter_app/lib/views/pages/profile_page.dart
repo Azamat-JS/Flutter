2234a9637ef01c8e9724f26ad1ef8bec984b29da
Azamat-JS <azamatielts987@gmail.com>
2025-12-23 22:43:13 +0500

    implemented hero-widget

--- flutter_app/lib/views/pages/profile_page.dart
+++ flutter_app/lib/views/pages/profile_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/data/notifiers.dart';
+import 'package:flutter_app/views/pages/welcome_page.dart';
+import 'package:flutter_app/views/widgets/hero-widget.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile Page'));
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Logout'),
+            leading: Icon(Icons.logout),
+            onTap: () {
+              selectedPageNotifier.value = 0;
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return WelcomePage();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
