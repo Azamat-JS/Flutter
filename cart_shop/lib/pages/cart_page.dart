906335320422c7b93361db9575a997dcf4e8d704
Azamat-JS <azamatielts987@gmail.com>
2026-01-18 01:38:00 +0500

    implemented pay now button in cart page

--- cart_shop/lib/pages/cart_page.dart
+++ cart_shop/lib/pages/cart_page.dart
@@ -1,11 +1,48 @@
-import 'package:cart_shop/components/my_drawer.dart';
+import 'package:cart_shop/components/my_button.dart';
+import 'package:cart_shop/models/product.dart';
+import 'package:cart_shop/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  void removeFromCart(BuildContext context, Product product) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text('Remove this item from cart?'),
+        actions: [
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+          MaterialButton(
+            onPressed: () {
+              Navigator.pop(context);
+              context.read<Shop>().removeFromCart(product);
+            },
+            child: Text('Delete'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void payButtonPressed(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        content: const Text('You can pay via different methods.'),
+        actions: [],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final cart = context.watch<Shop>().cart;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -13,7 +50,37 @@ class CartPage extends StatelessWidget {
         title: Text('Cart Page'),
       ),
       backgroundColor: Theme.of(context).colorScheme.surface,
-      drawer: MyDrawer(),
+      body: Column(
+        children: [
+          Expanded(
+            child: cart.isEmpty
+                ? const Center(child: Text('Your cart is empty.'))
+                : ListView.builder(
+                    itemCount: cart.length,
+                    itemBuilder: (context, index) {
+                      final item = cart[index];
+
+                      return ListTile(
+                        title: Text(item.name),
+                        subtitle: Text('\$' + item.price.toString()),
+                        trailing: IconButton(
+                          onPressed: () => removeFromCart(context, item),
+                          icon: Icon(Icons.remove_shopping_cart),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: MyButton(
+              onTap: () => payButtonPressed(context),
+              child: Text('Pay Now'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
