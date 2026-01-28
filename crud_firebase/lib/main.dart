455cb3f6b087fdbe6050e04a9815f225ff5b894f
Azamat-JS <azamatielts987@gmail.com>
2026-01-28 12:43:22 +0500

    implemeted firebase store and database

--- crud_firebase/lib/main.dart
+++ crud_firebase/lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:authentication/pages/auth_page.dart';
-import 'package:flutter/material.dart';
+import 'package:crud_firebase/firebase_options.dart';
+import 'package:crud_firebase/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,13 +12,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      home: HomePage(),
     );
   }
 }
-// 5:35:45
