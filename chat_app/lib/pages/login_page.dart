7103e27f2a6d231cff3dabf98fa853f18c0169d3
Azamat-JS <azamatielts987@gmail.com>
2026-01-31 11:35:27 +0500

    implemented lottie animated image in login page

--- chat_app/lib/pages/login_page.dart
+++ chat_app/lib/pages/login_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Theme.of(context).colorScheme.surface);
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/lotties/Login.json', height: 200),
+            const SizedBox(height: 10),
+            Text(
+              "Welcome back, Let's start to chat!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            TextField(),
+          ],
+        ),
+      ),
+    );
   }
 }
