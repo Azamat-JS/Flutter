b4c66ec91c7b815b1b7fef68d52315ad8893412b
Azamat-JS <azamatielts987@gmail.com>
2026-02-09 00:27:02 +0500

    implemented routes for each page

--- social_media_app/lib/main.dart
+++ social_media_app/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_app/auth/auth.dart';
+import 'package:social_media_app/auth/login_or_register.dart';
 import 'package:social_media_app/firebase_options.dart';
+import 'package:social_media_app/pages/home_page.dart';
+import 'package:social_media_app/pages/profile_page.dart';
+import 'package:social_media_app/pages/users_page.dart';
 import 'package:social_media_app/theme/dark_mode.dart';
 import 'package:social_media_app/theme/light_mode.dart';
 
@@ -21,8 +25,14 @@ class MyApp extends StatelessWidget {
       home: const AuthPage(),
       theme: lightMode,
       darkTheme: darkMode,
+      routes: {
+        '/login_register_page': (context) => const LoginOrRegister(),
+        '/home_page': (context) => HomePage(),
+        '/profile_page': (context) => ProfilePage(),
+        '/users_page': (context) => UsersPage(),
+      },
     );
   }
 }
 
-// 7:04:00
+// 7:17:00
--- social_media_app/lib/pages/users_page.dart
+++ social_media_app/lib/pages/users_page.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:social_media_app/components/my_drawer.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class UsersPage extends StatelessWidget {
+  const UsersPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('home'),
+        title: Text('users page'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 0,
-        actions: [],
       ),
-      drawer: MyDrawer(),
     );
   }
 }
