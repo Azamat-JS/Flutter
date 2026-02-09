8461fb0437c84d5510f7ed86930e288581b5cf62
Azamat-JS <azamatielts987@gmail.com>
2026-02-09 23:03:46 +0500

    implemented user's data in profile page

--- social_media_app/lib/pages/profile_page.dart
+++ social_media_app/lib/pages/profile_page.dart
@@ -1,7 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +21,25 @@ class ProfilePage extends StatelessWidget {
         title: Text('Profile page'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: getUserDetails(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          } else if (snapshot.hasData) {
+            Map<String, dynamic>? user = snapshot.data!.data();
+
+            return Column(
+              children: [Text(user!['email']), Text(user['username'])],
+            );
+          } else {
+            return Text("No Data");
+          }
+        },
       ),
     );
   }
