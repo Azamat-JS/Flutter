2e8da38be40b40f0099b70f4077d05bc3d379948
Azamat-JS <azamatielts987@gmail.com>
2026-02-10 15:47:45 +0500

    implemented text field in home page

--- social_media_app/lib/pages/home_page.dart
+++ social_media_app/lib/pages/home_page.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_app/components/my_drawer.dart';
+import 'package:social_media_app/components/my_textfield.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final TextEditingController newPostController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
-        title: Text('home'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('W A L L'),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         elevation: 0,
-        actions: [],
       ),
       drawer: MyDrawer(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25),
+            child: MyTextfield(
+              controller: newPostController,
+              hintText: "Say something...",
+              obscureText: false,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
