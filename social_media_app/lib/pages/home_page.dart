c4ed6fb6f6982b61e7f62731f167ed67357abb61
Azamat-JS <azamatielts987@gmail.com>
2026-02-10 16:10:23 +0500

    implemented add post functionality in home page

--- social_media_app/lib/pages/home_page.dart
+++ social_media_app/lib/pages/home_page.dart
@@ -2,11 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:social_media_app/components/my_drawer.dart';
 import 'package:social_media_app/components/my_post_button.dart';
 import 'package:social_media_app/components/my_textfield.dart';
+import 'package:social_media_app/database/firestore.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
 
+  final FirestoreDatabase database = FirestoreDatabase();
+
   final TextEditingController newPostController = TextEditingController();
+
+  void postMessage() {
+    if (newPostController.text.isNotEmpty) {
+      String message = newPostController.text;
+      database.addPost(message);
+    }
+
+    newPostController.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +45,7 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
 
-                MyPostButton(onTap: () {}),
+                MyPostButton(onTap: postMessage),
               ],
             ),
           ),
