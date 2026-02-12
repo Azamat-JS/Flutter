7797b84d7a67a74df88a757d16381c114f4d8247
Azamat-JS <azamatielts987@gmail.com>
2026-02-12 12:51:43 +0500

    implemented firebase api

--- notifications/lib/main.dart
+++ notifications/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notifications/api/firebase_api.dart';
 import 'package:notifications/firebase_options.dart';
 import 'package:notifications/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
 
