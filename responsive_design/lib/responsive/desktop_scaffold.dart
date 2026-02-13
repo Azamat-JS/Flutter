a08dfca5507cbb7a1f966d762de7bdfd379293b3
Azamat-JS <azamatielts987@gmail.com>
2026-02-13 18:16:59 +0500

    implemented responsive boxes in tablet and desktop screens

--- responsive_design/lib/responsive/desktop_scaffold.dart
+++ responsive_design/lib/responsive/desktop_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_design/constants/constants.dart';
+import 'package:responsive_design/util/my_box.dart';
+import 'package:responsive_design/util/my_tile.dart';
 
 class DesktopScaffold extends StatefulWidget {
   const DesktopScaffold({super.key});
@@ -14,7 +16,48 @@ class _DesktopScaffoldState extends State<DesktopScaffold> {
     return Scaffold(
       appBar: myAppBar,
       backgroundColor: myDefaultBackground,
-      body: Row(children: [myDrawer]),
+      body: Row(
+        children: [
+          myDrawer,
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                AspectRatio(
+                  aspectRatio: 4,
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: GridView.builder(
+                      itemCount: 4,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                          ),
+                      itemBuilder: (context, index) {
+                        return MyBox();
+                      },
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return MyTile();
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [Expanded(child: Container(color: Colors.blue))],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- responsive_design/lib/responsive/tablet_scaffold.dart
+++ responsive_design/lib/responsive/tablet_scaffold.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_design/constants/constants.dart';
+import 'package:responsive_design/util/my_box.dart';
+import 'package:responsive_design/util/my_tile.dart';
 
 class TabletScaffold extends StatefulWidget {
   const TabletScaffold({super.key});
@@ -15,6 +17,34 @@ class _TabletScaffoldState extends State<TabletScaffold> {
       appBar: myAppBar,
       drawer: myDrawer,
       backgroundColor: myDefaultBackground,
+      body: Column(
+        children: [
+          AspectRatio(
+            aspectRatio: 4,
+            child: SizedBox(
+              width: double.infinity,
+              child: GridView.builder(
+                itemCount: 4,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (context, index) {
+                  return MyBox();
+                },
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return MyTile();
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
