5ad917a679593648268d321e0152bdfe6477b999
Azamat-JS <azamatielts987@gmail.com>
2026-02-14 15:31:16 +0500

    implemented custom drawer widget and settings page

---
+++ song_app/lib/components/my_drawer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:song_app/pages/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+          DrawerHeader(child: Center(child: Icon(Icons.music_note, size: 40))),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 25),
+            child: ListTile(
+              title: const Text('H O M E'),
+              leading: const Icon(Icons.home),
+              onTap: () => Navigator.pop(context),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 25),
+            child: ListTile(
+              title: const Text('S E T T I N G S'),
+              leading: const Icon(Icons.settings),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SettingsPage()),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ song_app/lib/pages/settings_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:song_app/themes/theme_provider.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('S E T T I N G S')),
+      body: Container(
+        decoration: BoxDecoration(color: Theme.of(context).colorScheme.primary),
+        child: Row(
+          children: [
+            Text('Dark Mode'),
+            CupertinoSwitch(
+              value: Provider.of<ThemeProvider>(
+                context,
+                listen: false,
+              ).isDarkMode,
+              onChanged: (value) => Provider.of<ThemeProvider>(
+                context,
+                listen: false,
+              ).toggleTheme(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
