bf56c33f58c5018dabf0ba267fd3cae24226ff2c
Azamat-JS <azamatielts987@gmail.com>
2026-02-15 14:28:06 +0500

    implemented playlist configuration functions

--- song_app/lib/models/playlist_provider.dart
+++ song_app/lib/models/playlist_provider.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:song_app/models/podcast.dart';
 
@@ -26,11 +27,99 @@ class PlaylistProvider extends ChangeNotifier {
   int? _currentPodcastIndex;
 
   /*
+  Audio players
+  */
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  Duration _currentDuration = Duration.zero;
+  Duration _totalDuration = Duration.zero;
+
+  PlaylistProvider() {
+    listenToDuration();
+  }
+
+  bool _isPlaying = false;
+
+  void play() async {
+    final String path = _playlist[_currentPodcastIndex!].audioPath;
+    await _audioPlayer.stop();
+    await _audioPlayer.play(AssetSource(path));
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  void pause() async {
+    await _audioPlayer.pause();
+    _isPlaying = false;
+    notifyListeners();
+  }
+
+  void resume() async {
+    await _audioPlayer.resume();
+    _isPlaying = true;
+    notifyListeners();
+  }
+
+  void pauseOrResume() async {
+    if (_isPlaying) {
+      pause();
+    } else {
+      resume();
+    }
+    notifyListeners();
+  }
+
+  void seek(Duration position) async {
+    await _audioPlayer.seek(position);
+  }
+
+  void playNextPodcast() {
+    if (_currentPodcastIndex != null) {
+      if (_currentPodcastIndex! < _playlist.length - 1) {
+        currentPodcastIndex = _currentPodcastIndex! + 1;
+      } else {
+        currentPodcastIndex = 0;
+      }
+    }
+  }
+
+  void playPreviousPodcast() async {
+    if (_currentDuration.inSeconds > 2) {
+    } else {
+      if (_currentPodcastIndex! > 0) {
+        currentPodcastIndex = _currentPodcastIndex! - 1;
+      } else {
+        currentPodcastIndex = _playlist.length - 1;
+      }
+    }
+  }
+
+  void listenToDuration() {
+    _audioPlayer.onDurationChanged.listen((newDuration) {
+      _totalDuration = newDuration;
+      notifyListeners();
+    });
+
+    _audioPlayer.onDurationChanged.listen((newPosition) {
+      _currentDuration = newPosition;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPlayerComplete.listen((event) {
+      playNextPodcast();
+    });
+  }
+
+  /*
   gettters
   */
 
   List<Podcast> get playlist => _playlist;
   int? get currentPodcastIndex => _currentPodcastIndex;
+  bool get isPlaying => _isPlaying;
+  Duration get currentDuration => _currentDuration;
+  Duration get totalDuration => _totalDuration;
 
   /*
  setters
@@ -38,6 +127,10 @@ class PlaylistProvider extends ChangeNotifier {
 
   set currentPodcastIndex(int? newIndex) {
     _currentPodcastIndex = newIndex;
+
+    if (newIndex != null) {
+      play();
+    }
 
     notifyListeners();
   }
