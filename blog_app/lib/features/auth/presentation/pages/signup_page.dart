9581be6782dc711ec4e324532443242ee3dc7a86
Azamat-JS <azamatielts987@gmail.com>
2026-02-19 00:00:55 +0500

    implemented textfield validator

--- blog_app/lib/features/auth/presentation/pages/signup_page.dart
+++ blog_app/lib/features/auth/presentation/pages/signup_page.dart
@@ -11,43 +11,49 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    formKey.currentState!.validate();
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Sign Up.',
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            const AuthField(hintText: 'Name'),
-            const SizedBox(height: 15),
-            const AuthField(hintText: 'Email'),
-            const SizedBox(height: 15),
-            const AuthField(hintText: 'Password'),
-            const SizedBox(height: 20),
-            const AuthGradientButton(),
-            const SizedBox(height: 20),
-            RichText(
-              text: TextSpan(
-                text: "Don't have an account? ",
-                style: Theme.of(context).textTheme.titleMedium,
-                children: [
-                  TextSpan(
-                    text: 'Sign In',
-                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                      color: AppPallete.gradient2,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              const SizedBox(height: 30),
+              const AuthField(hintText: 'Name'),
+              const SizedBox(height: 15),
+              const AuthField(hintText: 'Email'),
+              const SizedBox(height: 15),
+              const AuthField(hintText: 'Password'),
+              const SizedBox(height: 20),
+              const AuthGradientButton(),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: "Don't have an account? ",
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        color: AppPallete.gradient2,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
