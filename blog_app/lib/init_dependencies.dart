c92f81a4991f7321bbb26ff7dff193a11fc69457
Azamat-JS <azamatielts987@gmail.com>
2026-02-22 15:26:07 +0500

    implemented Cubit increment function using flutter_bloc package

--- blog_app/lib/init_dependencies.dart
+++ blog_app/lib/init_dependencies.dart
@@ -1,19 +1,22 @@
 import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final serviceLocator = GetIt.instance;
 
 Future<void> initDependencies() async {
-  _initAuth();
   final supabase = await Supabase.initialize(
     url: AppSecrets.supabaseUrl,
     anonKey: AppSecrets.supabaseAnonKey,
   );
 
-  serviceLocator.registerLazySingleton(() => supabase.client);
+  serviceLocator.registerLazySingleton<SupabaseClient>(() => supabase.client);
+  _initAuth();
 }
 
 void _initAuth() {
@@ -21,5 +24,13 @@ void _initAuth() {
     () => AuthRemoteDataSourceImpl(serviceLocator()),
   );
 
-  serviceLocator.registerFactory(() => AuthRepositoryImpl(serviceLocator()));
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+
+  serviceLocator.registerFactory(() => UserSignUp(serviceLocator()));
+
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(userSignUp: serviceLocator()),
+  );
 }
