03deec4e5f7528a04abe47874c77ceab3099876f
Azamat-JS <azamatielts987@gmail.com>
2026-02-23 23:52:51 +0500

    created counter decrement function

--- bloc_tutorial/lib/bloc/counter_bloc.dart
+++ bloc_tutorial/lib/bloc/counter_bloc.dart
@@ -2,10 +2,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterIncremented {}
 
-class CounterBloc extends Bloc<CounterIncremented, int> {
+class CounterDecremented {}
+
+class CounterBloc extends Bloc<dynamic, int> {
   CounterBloc() : super(0) {
     on<CounterIncremented>((event, emit) {
       emit(state + 1);
+    });
+
+    on<CounterDecremented>((event, emit) {
+      if (state == 0) return;
+      emit(state - 1);
     });
   }
 }
