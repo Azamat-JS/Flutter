38aa37dd1aa3d7af5b6090494d93ac572e405108
Azamat-JS <azamatielts987@gmail.com>
2026-02-25 23:11:05 +0500

    started login form app using bloc

---
+++ bloc_login_form/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_login_form/login_screen.dart';
+import 'package:bloc_login_form/pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
