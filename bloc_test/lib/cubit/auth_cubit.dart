f3367578ff325029bdfe62d2385fa14261f97b2f
Azamat-JS <azamatielts987@gmail.com>
2026-03-02 12:11:19 +0500

    implemented auth state

--- bloc_test/lib/cubit/auth_cubit.dart
+++ bloc_test/lib/cubit/auth_cubit.dart
@@ -1,11 +1,34 @@
+import 'package:bloc_test/cubit/auth_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class AuthCubit extends Cubit<bool> {
-  AuthCubit() : super(false);
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
 
-  void login() => emit(true);
+  Future<void> login(String email, String password) async {
+    emit(AuthLoading());
 
-  void logout() => emit(false);
+    try {
+      await Future.delayed(const Duration(seconds: 2));
 
-  void signUp() => emit(true);
+      if (email == 'test@gmail.com' && password == '123456') {
+        emit(AuthAuthenticated(email));
+      } else {
+        emit(AuthError('Invalid credentials'));
+      }
+    } catch (e) {
+      emit(AuthError('An unexpected error occurred'));
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 2));
+      if (email.isNotEmpty && password.isNotEmpty) {
+        emit(AuthAuthenticated(email));
+      } else {
+        emit(AuthError('Email and password cannot be empty'));
+      }
+    } catch (e) {}
+  }
 }
