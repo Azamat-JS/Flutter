23d8b9e8a5e8bbb91d2b10fcee347fe4498a030c
Azamat-JS <azamatielts987@gmail.com>
2026-03-03 19:49:25 +0500

    implemeted add todo dialog

--- bloc_test_todo/lib/main.dart
+++ bloc_test_todo/lib/main.dart
@@ -7,11 +7,13 @@ import 'package:bloc_test_todo/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  final dir = await getApplicationDocumentsDirectory();
 
-  final isar = await Isar.open([TodoModelSchema], directory: './isar');
+  final isar = await Isar.open([TodoModelSchema], directory: dir.path);
   final local = TodoLocalDatasourceImpl(isar);
   final repository = TodoRepositoryImpl(local);
   runApp(MyApp(repository: repository));
@@ -26,6 +28,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => TodoBloc(repository)..add(LoadTodos()),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Todo App',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- bloc_test_todo/lib/presentation/pages/home_page.dart
+++ bloc_test_todo/lib/presentation/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_test_todo/presentation/bloc/todo_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,11 +10,88 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final titleController = TextEditingController();
+
+  void addTodo() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add ToDo'),
+        content: TextField(
+          autofocus: true,
+          controller: titleController,
+          onSubmitted: (value) {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Cancel'),
+              ),
+              SizedBox(width: 5),
+              ElevatedButton(
+                onPressed: () {
+                  context.read<TodoBloc>().add(
+                    AddTodo(titleController.text.trim()),
+                  );
+                  titleController.clear();
+                  Navigator.of(context).pop();
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Todo App")),
-      body: const Center(child: Text("Hello, World!")),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addTodo,
+        child: Icon(Icons.add),
+      ),
+      body: BlocBuilder<TodoBloc, TodoState>(
+        builder: (context, state) {
+          if (state is TodoLoading) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          }
+          if (state is TodoLoaded) {
+            if (state.todos.isEmpty) {
+              return const Center(child: Text('No Todos yet'));
+            }
+            return ListView.builder(
+              itemCount: state.todos.length,
+              itemBuilder: (context, index) {
+                final todo = state.todos[index];
+                return ListTile(
+                  title: Text(todo.title),
+                  trailing: Checkbox(
+                    value: todo.isCompleted,
+                    onChanged: (_) {
+                      context.read<TodoBloc>().add(ToggleTodo(todo.id));
+                    },
+                  ),
+                );
+              },
+            );
+          }
+          if (state is TodoError) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+          return const SizedBox();
+        },
+      ),
     );
   }
 }
