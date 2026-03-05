9699d844f35cfe888543fe8c69be5d4ab7e81eae
Azamat-JS <azamatielts987@gmail.com>
2026-03-05 20:06:30 +0500

    implemented auth repository impl

--- bloc_test_todo/lib/data/repositories/auth_repo_impl.dart
+++ bloc_test_todo/lib/data/repositories/auth_repo_impl.dart
@@ -1,3 +1,28 @@
+import 'package:bloc_test_todo/data/datasources/auth_local_datasource.dart';
 import 'package:bloc_test_todo/domain/repositories/auth_repo.dart';
 
-class AuthRepositoryImpl extends AuthRepository {}
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthLocalDatasource local;
+
+  AuthRepositoryImpl(this.local);
+
+  @override
+  Future<void> signUp(String username, String email, String password) async {
+    await local.signUp(username: username, email: email, password: password);
+  }
+
+  @override
+  Future<void> login(String email, String password) async {
+    await local.login(email: email, password: password);
+  }
+
+  @override
+  Future<void> logout() async {
+    await local.logout();
+  }
+
+  @override
+  Future<bool> isLoggedIn() async {
+    return await local.isLoggedIn();
+  }
+}
