000510ba9d62999c5404559b07bde5c62015c992
Azamat-JS <azamatielts987@gmail.com>
2026-03-10 20:08:38 +0500

    removed blog app

--- blog_app/lib/core/secrets/app_secrets.dart
+++
@@ -1,5 +0,0 @@
-class AppSecrets {
-  static const String supabaseUrl = "https://irfdmtqsaxsrrimbcwby.supabase.co";
-  static const String supabaseAnonKey =
-      "sb_publishable_D8N_iZleJDxK_bPSqsb4tA_wzrPTE3t";
-}
--- blog_app/lib/core/usecase/usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:blog_app/core/error/failures.dart';
-import 'package:fpdart/fpdart.dart';
-
-abstract interface class Usecase<SuccessType, Params> {
-  Future<Either<Failure, SuccessType>> call(Params params);
-}
