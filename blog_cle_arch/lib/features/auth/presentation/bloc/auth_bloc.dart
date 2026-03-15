dfb627501a4936fe99140ced4fe0dc1673b7a8c3
Azamat-JS <azamatielts987@gmail.com>
2026-03-15 16:43:20 +0500

    implemented AuthLogin event in auth bloc

--- blog_cle_arch/lib/features/auth/presentation/bloc/auth_bloc.dart
+++ blog_cle_arch/lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:blog_cle_arch/features/auth/domain/entities/user_entity.dart';
+import 'package:blog_cle_arch/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_cle_arch/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
@@ -7,23 +8,38 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final UserSignUp _userSignUp;
-  AuthBloc({required UserSignUp userSignUp})
+  final UserLogin _userLogin;
+  AuthBloc({required UserSignUp userSignUp, required UserLogin userLogin})
     : _userSignUp = userSignUp,
+      _userLogin = userLogin,
       super(AuthInitial()) {
-    on<AuthSignUp>((event, emit) async {
-      emit(AuthLoading());
-      final res = await _userSignUp.call(
-        UserSignUpParams(
-          name: event.name,
-          email: event.email,
-          password: event.password,
-        ),
-      );
+    on<AuthSignUp>(_onAuthSignUp);
 
-      res.fold(
-        (l) => emit(AuthFailure(l.message)),
-        (userData) => emit(AuthSuccess(userData)),
-      );
-    });
+    on<AuthLogin>(_onAuthLogin);
+  }
+
+  void _onAuthSignUp(AuthSignUp event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final res = await _userSignUp.call(
+      UserSignUpParams(
+        name: event.name,
+        email: event.email,
+        password: event.password,
+      ),
+    );
+
+    res.fold(
+      (l) => emit(AuthFailure(l.message)),
+      (userData) => emit(AuthSuccess(userData)),
+    );
+  }
+
+  void _onAuthLogin(AuthLogin event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    final res = await _userLogin(
+      UserLoginParams(email: event.email, password: event.password),
+    );
+
+    res.fold((l) => emit(AuthFailure(l.message)), (r) => AuthSuccess(r));
   }
 }
