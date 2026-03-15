2b064f2ec45a2db0a1bca65666011c01b719a09b
Azamat-JS <azamatielts987@gmail.com>
2026-03-15 14:44:50 +0500

    implemented user model and user entity

---
+++ blog_cle_arch/lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,5 @@
+import 'package:blog_cle_arch/features/auth/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({required super.id, required super.email, required super.name});
+}
