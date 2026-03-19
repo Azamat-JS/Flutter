5cf9a59d0c8da8b871c2db1114a54e5574da410e
Azamat-JS <azamatielts987@gmail.com>
2026-03-19 12:35:50 +0500

    styles given to placeholder

--- blog_cle_arch/lib/features/blog/presentation/widgets/blog_editor.dart
+++ blog_cle_arch/lib/features/blog/presentation/widgets/blog_editor.dart
@@ -1,3 +1,4 @@
+import 'package:blog_cle_arch/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class BlogEditor extends StatelessWidget {
@@ -13,7 +14,13 @@ class BlogEditor extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      decoration: InputDecoration(hintText: hintText),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+          color: AppPallete.greyColor,
+          fontStyle: FontStyle.italic,
+        ),
+      ),
       maxLines: null,
     );
   }
