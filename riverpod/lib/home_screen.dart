bca475813fb7a1c8ba38df84982ef088800a2017
Azamat-JS <azamatielts987@gmail.com>
2026-09-23 04:36:55 +0500

    implemented riverpod methods

---
+++ riverpod/lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:podtestriver/main.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Consumer(
+        builder: (context, ref, child) {
+          final name = ref.watch(nameProvider);
+          return Column(children: [Center(child: Text(name))]);
+        },
+      ),
+    );
+  }
+}
---
+++ riverpod/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:podtestriver/home_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nameProvider = Provider<String>((ref) => 'Azamat Abdulloh');
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Riverpod tutorial',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
